7ecdb178b6d41894d70885d4e568ef3ddbe1fe0c
jjcollinge <jonathancollinge@live.com>
2017-05-04 11:16:48 +0100

    Added intiail asp.net core web app

---
+++ web/Models/EthereumSettings.cs
@@ -0,0 +1,14 @@
+namespace web.Models
+{
+    public class EthereumSettings
+    {
+        public EthereumSettings()
+        {
+        }
+        public string EthereumAccount { get; set; }
+        public string EthereumPassword { get; set; }
+        public string StorageKey { get; set; }
+        public string StorageAccount { get; set; }
+
+    }
+}
---
+++ web/Services/IEthereumService.cs
@@ -0,0 +1,17 @@
+using System.Threading.Tasks;
+using web.Models;
+using Nethereum.Contracts;
+
+namespace web.Services
+{
+    public interface IEthereumService
+    {
+        string AccountAddress { get; set; }
+        Task<bool> SaveContractToTableStorage(EthereumContractInfo contract);
+        Task<EthereumContractInfo> GetCOntractFromTableStorage(string name);
+        Task<decimal> GetBalance(string address);
+        Task<bool> ReleaseContract(string name, string abi, string byteCode, int gas);
+        Task<string> TryGetContractAddress(string name);
+        Task<Contract> GetContract(string name);
+    }
+}
