79931b8c3446ec7de9f6188df4390e85d0070b96
jjcollinge <jonathancollinge@live.com>
2017-05-04 12:43:46 +0100

    Fixed typo

--- web/Services/IEthereumService.cs
+++ web/Services/IEthereumService.cs
@@ -8,7 +8,7 @@ namespace web.Services
     {
         string AccountAddress { get; set; }
         Task<bool> SaveContractToTableStorage(EthereumContractInfo contract);
-        Task<EthereumContractInfo> GetCOntractFromTableStorage(string name);
+        Task<EthereumContractInfo> GetContractFromTableStorage(string name);
         Task<decimal> GetBalance(string address);
         Task<bool> ReleaseContract(string name, string abi, string byteCode, int gas);
         Task<string> TryGetContractAddress(string name);
